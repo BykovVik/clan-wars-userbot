11c990a30585e556b8abe6057fd3db1b31470701
BykovVik <piton20091986@gmail.com>
2024-02-03 10:23:57 +0300

    first commit

---
+++ app/config.py
@@ -0,0 +1,7 @@
+from dotenv import load_dotenv
+import os
+from pyrogram import Client
+
+load_dotenv()
+userbot = Client('bot', os.getenv("API_ID"), os.getenv("API_HASH"), phone_number=os.getenv("PHONE"))
+
---
+++ app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from app.config import userbot
+
+app = FastAPI()
+
+# Counting the number of reactions
+@app.get("/messages/{chat_id}/{message_id}")
+async def get_reactions(chat_id: int, message_id: int):
+    # Searching for the required message via the telegram API
+    await userbot.start()
+    messages = await userbot.get_messages(chat_id=chat_id, message_ids=message_id)
+    await userbot.stop()
+    count = [0 + c.count for c in messages.reactions.reactions]
+
+    return {'reactions_count': sum(count)}
+    
+
+
+
+
+    
